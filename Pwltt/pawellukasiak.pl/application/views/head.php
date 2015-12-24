508df824611ee1ea0f8a62c70e6091044a3910ed
pwltt <pablo1991lol@gmail.com>
2015-12-24 13:48:35 +0100

    fixed Index.php in url

--- Pwltt/pawellukasiak.pl/application/views/head.php
+++ Pwltt/pawellukasiak.pl/application/views/head.php
@@ -7,6 +7,7 @@
         <link href="<?php echo base_url()?>assets/css/bootstrap.css" rel="stylesheet" type="text/css"/>
               <link href="<?php echo base_url()?>assets/js/jquery-ui-1.11.4/jquery-ui.css" rel="stylesheet" type="text/css"/>
               <link href="<?php echo base_url()?>assets/css/style.css" rel="stylesheet" type="text/css"/>
+              <link href="<?php echo base_url()?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
               
               <script src="<?php echo base_url()?>assets/js/jquery-ui-1.11.4/external/jquery/jquery.js" type="text/javascript"></script>
               <script src="<?php echo base_url()?>assets/js/jquery-ui-1.11.4/jquery-ui.js" type="text/javascript"></script>
--- Pwltt/pawellukasiak.pl/application/views/navBar.php
+++ Pwltt/pawellukasiak.pl/application/views/navBar.php
@@ -18,35 +18,44 @@
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Link<span class="sr-only">(current)</span></a></li>
-              <li><a href="#">Link</a></li>
-              
+                
+                <li><a href="https://github.com/pwltt/MES-PWLTT-ENVI/tree/master/Pwltt/pawellukasiak.pl" 
+                       class="fa fa-github-square" style="font-size: xx-large; cursor: pointer"></a></li>
+                       
             </ul>
+              
+            <!-- PASEK SEARCH ----------------------------------- -->
             <form class="navbar-form navbar-left" role="search">
               <div class="form-group">
                 <input type="text" class="form-control" placeholder="Wpisz słowo">
               </div>
               <button type="submit" class="btn btn-default">Szukaj</button>
             </form>
+            <!-- KONIEC PASEK SEARCH --------------------------- -->
+            
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Link</a></li>
+                
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                     <span class="caret"></span> 
-                    <span class="glyphicon glyphicon-cog" aria-hidden="true" style="font-size: x-large" > </span>
+                    <span class="glyphicon glyphicon-user" aria-hidden="true" style="font-size: x-large" ></span>
                 </a>
                 <ul class="dropdown-menu">
                     <?php
                     if ($this->session->userdata('logged_in') == TRUE)
                     { ?>
+                        <li> <a class="glyphicon glyphicon-cog" aria-hidden="true" style="font-size: medium; cursor: pointer">  Ustawienia </a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
                         <li><a href="page/logout" style="cursor: pointer">Logout</a></li>
                     <?php
                     } else{   ?>
                         <li><a id="login_button" style="cursor: pointer">Zaloguj</a></li>
                         <li><a id="register" style="cursor: pointer">Zarejestruj</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
                     <?php } ?>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
+                    
                 </ul>
               </li>
             </ul>
